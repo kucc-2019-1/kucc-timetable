1a77ae2549336f53902210791009504fe523b8db
Gaeun Kim <gaeunkim981004@gmail.com>
2019-03-31 19:53:16 +0900

    [frontend] 날짜 selector 추가

---
+++ frontend/src/components/DateTimePicker.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="datepicker">
+<!--      <v-date-picker v-model="picker" :landscape="landscape" :reactive="reactive"></v-date-picker>-->
+      <v-select
+        :items="days"
+        label="날짜"
+      ></v-select>
+    </div>
+</template>
+
+<script>
+    export default {
+      el:'#datepicker',
+      name: "DateTimePicker",
+      data () {
+        return {
+          picker: new Date().toISOString().substr(0,10),
+          landscape: false,
+          reactive: false,
+          days: ['2019-03-30','2019-03-31'],
+          dates: [
+            {
+              day: '2019-03-30',
+              times: [
+                '9:00~9:30',
+                '9:30~10:00'
+              ]
+            },
+            {
+              day: '2019-03-31',
+              times: [
+                '9:00~9:30',
+                '9:30~10:00',
+                '13:00~13:30'
+              ]
+            }
+          ]
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/DateTimePicker'
+import 'vuetify/dist/vuetify.min.css'
+import DateTimePicker from "../components/DateTimePicker";
 
 Vue.use(Router)
+Vue.use(Vuetify)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'DateTimePicker',
+      component: DateTimePicker
     }
   ]
 })
