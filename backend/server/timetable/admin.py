e354c821922658ce21b14772c4c3cfa11009b385
Woo <wgm0601@gmail.com>
2019-04-02 23:49:53 +0900

    django administration 추가

--- backend/server/timetable/admin.py
+++ backend/server/timetable/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Reservation, TimeIndex, TimeTable
+
+
+admin.site.register(Reservation)
+admin.site.register(TimeIndex)
+admin.site.register(TimeTable)
