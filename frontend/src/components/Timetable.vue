8683660383197d07dbca7197aceec68705767a98
kucc314 <anamkucc314@gmail.com>
2019-04-12 00:05:10 +0900

    [frontend] 예약 내역이 시간표에 텍스트로 표시되도록 함

--- frontend/src/components/Timetable.vue
+++ frontend/src/components/Timetable.vue
@@ -1,20 +1,16 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
     <v-data-table
       :headers="dayHeaders"
-      :items="times"
-      :total-items="times.length"
+      :items="rows"
+      :total-items="rows.length"
       hide-actions
       class="elevation-1"
     >
       <template v-slot:items="props">
         <td>{{ props.item.time }}</td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <template v-for="day in 7">
+          <td v-html="getReservationText(findReservation(props.item.reservations, day-1))"></td>
+        </template>
       </template>
     </v-data-table>
 </template>
@@ -52,13 +48,31 @@
         }];
         return header.concat(this.days.map(getDateHeader));;
       },
-      times: function() {
+      rows: function() {
         const result = [];
 
         for (let time = this.startTime; time < this.endTime-1; time++) {
-          result.push({time: `${time}:00~${time+1}:00`})
+          let row = { time: `${time}:00~${time+1}:00` };
+          row['reservations'] = this.reservations
+                                    .filter(reservation => this.isTimeIn(reservation, time - this.startTime));
+          result.push(row)
         }
         return result;
+      }
+    },
+    methods: {
+      findReservation: function(reservations, day) {
+        let result = reservations.filter(reservation => reservation.day === day);
+        return result ? result[0] : null;
+      },
+      getReservationText: function(reservation) {
+        if (reservation) {
+          return `${reservation.title} <br/> ${reservation.name}`;
+        }
+        return '';
+      },
+      isTimeIn: function(reservation, time) {
+        return reservation.start_time <= time && time <= reservation.end_time;
       }
     },
     data() {
@@ -66,6 +80,36 @@
         startDate: new Date('2019-04-10'),
         startTime: 9,
         endTime: 23,
+        reservations: [
+          {
+            "title" : "python 스터디1",
+            "name" : "김수홍",
+            "day" : 0,
+            "start_time": 0,
+            "end_time":2,
+          },
+          {
+            "title" : "python 스터디2",
+            "name" : "김수홍2",
+            "day" : 0,
+            "start_time": 5,
+            "end_time": 5,
+          },
+          {
+            "title" : "python 스터디3",
+            "name" : "김수홍3",
+            "day" : 5,
+            "start_time":3,
+            "end_time":4,
+          },
+          {
+            "title" : "python 스터디4",
+            "name" : "김수홍4",
+            "day" : 3,
+            "start_time":2,
+            "end_time":3,
+          }
+        ]
       }
     }
   }
