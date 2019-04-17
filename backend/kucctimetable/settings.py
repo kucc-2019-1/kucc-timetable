d76af172848105e6849a2027c7e2989054f2991f
Jack <rainy_waltz@naver.com>
2019-04-17 11:08:28 +0900

    로그인 템플릿 틀 제작

--- backend/kucctimetable/settings.py
+++ backend/kucctimetable/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'kucctimetable.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'user.User'
+
+LOGIN_REDIRECT_URL = '/'
