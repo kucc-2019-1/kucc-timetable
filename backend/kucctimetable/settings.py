5230953f1c3c52a6c4644dc08468799d53e17ed0
kucc314 <anamkucc314@gmail.com>
2019-05-30 21:52:53 +0900

    [backend] CORS 미들웨어 위치 변경

--- backend/kucctimetable/settings.py
+++ backend/kucctimetable/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'kucctimetable.urls'
